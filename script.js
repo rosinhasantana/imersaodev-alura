b2f1218a2bd83fd09ffb53495d51a851018fd2af
rosinhasantana <99768671+rosinhasantana@users.noreply.github.com>
2022-03-14 18:23:18 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function Converter() {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+  var valorEmDolarNumerico = parseFloat(valor);
+
+  var valorEmReal = valorEmDolarNumerico * 5.13;
+  console.log(valorEmReal);
+
+  var elementoValorConvertido = document.getElementById("valorConvertido");
+  var valorConvertido = "O resultado em real é R$" + valorEmReal;
+  elementoValorConvertido.innerHTML = valorConvertido;
+}
